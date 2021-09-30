4bd44e5fde6817e68cf84ae2579d106bfba10a09
Kiwi <Kiwi@DESKTOP-RSF2NI2>
2021-09-30 18:35:28 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Team5_ConestogaVirtualGameStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
