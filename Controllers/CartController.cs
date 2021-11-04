17030e71bb778155ca1ab730db27c50344ef6fbc
heyruyn23 <heyruyn23@gmail.com>
2021-11-04 16:55:47 -0400

    Game, Role, Detail Added

---
+++ Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team5_ConestogaVirtualGameStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ILogger<CartController> _logger;
+
+        public CartController(ILogger<CartController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/GameController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team5_ConestogaVirtualGameStore.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly ILogger<GameController> _logger;
+
+        public GameController(ILogger<GameController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Store()
+        {
+            return View();
+        }
+
+        public IActionResult GameDetail()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult AddGame()
+        {
+            return View();
+        }
+
+        // find something by id
+        // role based control do it here
+    }
+}
