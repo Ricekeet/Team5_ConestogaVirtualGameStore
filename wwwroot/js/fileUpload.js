fd318d9ca9e64fc833c13b8a14d45e96c0bb7cfe
heyruyn23 <heyruyn23@gmail.com>
2021-11-25 15:56:21 -0500

    removed alert testing

--- wwwroot/js/fileUpload.js
+++ wwwroot/js/fileUpload.js
@@ -3,5 +3,5 @@ $(".custom-file-input").on("change", function () {
     var fileName = $(this).val().split("\\").pop();
     $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
     var htmlVal = $(this).siblings(".custom-file-label").html();
-    alert(fileName + " " + htmlVal);
+    //alert(fileName + " " + htmlVal);
 });
