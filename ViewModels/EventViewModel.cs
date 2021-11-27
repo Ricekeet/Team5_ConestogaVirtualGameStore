d3c65a862a4a5457c9debc6ff4bcb6ab1c7a5bee
heyruyn23 <heyruyn23@gmail.com>
2021-11-27 15:05:43 -0500

    1127

---
+++ ViewModels/EventViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team5_ConestogaVirtualGameStore.ViewModels
+{
+    public class EventViewModel
+    {
+        public int EventId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Description { get; set; }
+        [Required]
+        [DataType(DataType.Upload)]
+        public IFormFile EventPic { get; set; }
+    }
+}
